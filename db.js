aee2a0efd59bc71f7fe3cb74076e6aae666154a5
Priya <dev@yesgobus.com>
2026-09-15 11:45:00 +0530

    Move database credentials to environment variables

--- db.js
+++ db.js
@@ -1,19 +1,14 @@
+require("dotenv").config();
 const mysql = require("mysql2");
 
-const db = mysql.createPool({
-  host: "localhost",
-  user: "root",
-  password: "MySQL@12345",
-  database: "hrms_db"
-});
+require("dotenv").config();
 
-db.getConnection((err, connection) => {
-  if (err) {
-    console.error("MySQL connection failed:", err.message);
-  } else {
-    console.log("MySQL connected successfully");
-    connection.release();
-  }
+const db = mysql.createPool({
+  host: process.env.DB_HOST,
+  user: process.env.DB_USER,
+  password: process.env.DB_PASSWORD,
+  database: process.env.DB_NAME,
+  port: process.env.DB_PORT
 });
 
 module.exports = db;
